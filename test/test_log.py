83357cc72e46057088c9041a45bd90bd339d228e
tzing <tzingshih@gmail.com>
2021-05-02 21:35:14 +0800

    wip. fix test

--- test/test_log.py
+++ test/test_log.py
@@ -1,46 +1,53 @@
 import logging
 import re
+import unittest
 import unittest.mock
-
-import pytest
 
 import livy.client
 import livy.log as module
 
 
-def test_default_parser():
-    pattern = re.compile(
-        r"^(\d{2}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2}) ([A-Z]+) (.+?):(.*(?:\n\t.+)*)"
-    )
+class LivyBatchLogReaderTester(unittest.TestCase):
+    def setUp(self) -> None:
+        self.client = unittest.mock.MagicMock(spec=livy.client.LivyClient)
+        self.reader = module.LivyBatchLogReader(self.client, 1234)
 
-    m = pattern.match("21/05/01 12:34:56 DEBUG Foo: test message")
-    p = module.default_parser(m)
+    def test___init__(self):
+        # only test fail
+        with self.assertRaises(TypeError):
+            module.LivyBatchLogReader(object(), 1234)
+        with self.assertRaises(TypeError):
+            module.LivyBatchLogReader(self.client, "1234")
 
-    assert isinstance(p, module.LivyLogParseResult)
-    assert p.created == 1619843696
-    assert p.level == logging.DEBUG
-    assert p.name == "Foo"
-    assert p.message == "test message"
+    def test_add_parsers(self):
+        # success
+        pattern = re.compile(r"(.+): (.+)")  # dummy
+        self.reader.add_parsers(pattern, module.default_parser)
 
-
-def test_convert_stdout():
-    p = module.convert_stdout("Foo")
-
-    assert isinstance(p, module.LivyLogParseResult)
-    assert p.message == "Foo"
-
-
-@pytest.fixture
-def client():
-    return unittest.mock.MagicMock(spec=livy.client.LivyClient)
+        # fail
+        with self.assertRaises(TypeError):
+            self.reader.add_parsers(r"(.+): (.+)", module.default_parser)
+        with self.assertRaises(TypeError):
+            self.reader.add_parsers(pattern, "1234")
 
 
-def test_LivyBatchLogReader_init(client):
-    # success
-    module.LivyBatchLogReader(client, 1234)
+class ParserTester(unittest.TestCase):
+    def test_default_parser(self):
+        pattern = re.compile(
+            r"^(\d{2}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2}) ([A-Z]+) (.+?):(.*(?:\n\t.+)*)"
+        )
 
-    # fail
-    with pytest.raises(TypeError):
-        module.LivyBatchLogReader(object(), 1234)
-    with pytest.raises(TypeError):
-        module.LivyBatchLogReader(client, "1234")
+        m = pattern.match("21/05/01 12:34:56 DEBUG Foo: test message")
+        p = module.default_parser(m)
+
+        assert isinstance(p, module.LivyLogParseResult)
+        assert p.created == 1619843696
+        assert p.level == logging.DEBUG
+        assert p.name == "Foo"
+        assert p.message == "test message"
+
+    def test_convert_stdout(self):
+        p = module.convert_stdout("Foo")
+
+        assert isinstance(p, module.LivyLogParseResult)
+        assert p.message == "Foo"
