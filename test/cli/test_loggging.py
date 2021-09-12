b82e09679a8ceb02cbd844faf2ec6134edf9ca80
tzing <tzingshih@gmail.com>
2021-09-12 17:41:11 +0800

    chore: rewrite test

--- test/cli/test_loggging.py
+++ test/cli/test_loggging.py
@@ -3,130 +3,150 @@ import importlib.util
 import logging
 import os
 import tempfile
-import time
-import typing
+import unittest
 import unittest.mock
-
-import pytest
 
 import livy.cli.logging as module
 
-
-def test_setup_argparse():
-    p = argparse.ArgumentParser()
-    module.setup_argparse(p, True)
-    p.parse_args(["-q"])
+# some package are optional; they are not installed in `test-core`
+no_colorama = importlib.util.find_spec("colorama") is None
 
 
-@pytest.mark.parametrize("with_display_feature", [True, False])
-@unittest.mock.patch("livy.cli.logging._is_initialized", False)
-def test_init_with_display(with_display_feature):
-    args = argparse.Namespace()
-    args.verbose = 0
-    args.log_file = True
+class TestArgumentParse(unittest.TestCase):
+    def tearDown(self) -> None:
+        module._is_initialized = False
 
-    if with_display_feature:
+    def test_setup_argparse(self):
+        p = argparse.ArgumentParser()
+        module.setup_argparse(p, True)
+        p.parse_args(["-q"])
+
+    def test_init(self):
+        args = argparse.Namespace()
+        args.verbose = 0
+        args.log_file = True
+
+        with tempfile.NamedTemporaryFile() as fp, unittest.mock.patch(
+            "os.getcwd", return_value=os.path.dirname(fp.name)
+        ):
+            module.init(args)
+
+        module.init(args)  # test cache
+
+    def test_init_with_display(self):
+        args = argparse.Namespace()
+        args.verbose = 0
+        args.log_file = True
         args.highlight_logger = ["test-highlight-logger"]
         args.hide_logger = ["test-hide-logger"]
 
-    with tempfile.NamedTemporaryFile() as fp, unittest.mock.patch(
-        "os.getcwd", return_value=os.path.dirname(fp.name)
-    ):
-        module.init(args)
-
-    module.init(args)
-
-
-@pytest.fixture
-def stream():
-    s = unittest.mock.MagicMock()
-    return s
+        # fmt: off
+        with tempfile.NamedTemporaryFile() as fp, \
+            unittest.mock.patch("os.getcwd", return_value=os.path.dirname(fp.name)), \
+            unittest.mock.patch("livy.cli.logging._get_console_handler", return_value=[logging.StreamHandler(), "Test message"]):
+            module.init(args)
+        # fmt: on
 
 
-def test__get_console_formatter_colored(stream: typing.TextIO):
-    if not importlib.util.find_spec("colorama"):  # test-core does not install colorama
-        return
+class TestFormatter(unittest.TestCase):
+    def setUp(self) -> None:
+        self.stream = unittest.mock.MagicMock()
+        self.stream.isatty.return_value = True
 
-    stream.isatty.return_value = True
-    formatter = module._get_console_formatter(stream)
-    assert isinstance(formatter, logging.Formatter)
+        self.record = logging.makeLogRecord(
+            {
+                "name": "Test.Bar",
+                "levelno": logging.INFO,
+                "levelname": "INFO",
+                "msg": "Test log message",
+                "created": 1631440284,
+            }
+        )
 
+    @unittest.skipIf(no_colorama, "colorama is not installed")
+    def test_get_console_formatter(self):
+        assert isinstance(module._get_console_formatter(self.stream), logging.Formatter)
 
-@pytest.fixture
-def record() -> logging.LogRecord:
-    return logging.makeLogRecord(
-        {
-            "name": "Test.Bar",
-            "levelno": logging.INFO,
-            "levelname": "INFO",
-            "msg": "Test log message",
-            "created": (time.time()),
-        }
-    )
+    def test_get_console_formatter_fallback(self):
+        self.stream.isatty.return_value = False
+        assert isinstance(module._get_console_formatter(self.stream), logging.Formatter)
 
+    @unittest.skipIf(no_colorama, "colorama is not installed")
+    @unittest.mock.patch("livy.cli.logging._is_wanted_logger")
+    def test_ColoredFormatter(self, is_wanted_logger):
+        formatter = module._ColoredFormatter(
+            "%(levelcolor)s%(asctime)s %(name)s:%(reset)s %(message)s",
+            "%Y-%m-%d %H:%M:%S %z",
+        )
 
-def test__ColoredFormatter(record: logging.LogRecord):
-    if not importlib.util.find_spec("colorama"):  # test-core does not install colorama
-        return
+        is_wanted_logger.return_value = True
+        formatter.format(self.record)
 
-    formatter = module._ColoredFormatter(
-        "%(levelcolor)s%(asctime)s %(name)s:%(reset)s %(message)s",
-        "%Y-%m-%d %H:%M:%S %z",
-    )
+        is_wanted_logger.return_value = False
+        formatter.format(self.record)
 
-    with unittest.mock.patch("livy.cli.logging._is_wanted_logger", return_value=True):
-        formatter.format(record)
+    def test_is_wanted_logger(self):
+        wantted_names = {"Foo.Bar", "Baz"}
 
-    with unittest.mock.patch("livy.cli.logging._is_wanted_logger", return_value=False):
-        formatter.format(record)
+        self.record.name = "Foo.Bar"
+        assert module._is_wanted_logger(self.record, wantted_names)
 
+        self.record.name = "Foo.Bar.Baz"
+        assert module._is_wanted_logger(self.record, wantted_names)
 
-def test__is_wanted_logger(record: logging.LogRecord):
-    wantted_names = {"Foo.Bar", "Baz"}
+        self.record.name = "Baz"
+        assert module._is_wanted_logger(self.record, wantted_names)
 
-    record.name = "Foo.Bar"
-    assert module._is_wanted_logger(record, wantted_names)
+        self.record.name = "Baz.Foo"
+        assert module._is_wanted_logger(self.record, wantted_names)
 
-    record.name = "Foo.Bar.Baz"
-    assert module._is_wanted_logger(record, wantted_names)
+        self.record.name = "Foo"
+        assert not module._is_wanted_logger(self.record, wantted_names)
 
-    record.name = "Baz"
-    assert module._is_wanted_logger(record, wantted_names)
+        self.record.name = "Foo.BarBaz"
+        assert not module._is_wanted_logger(self.record, wantted_names)
 
-    record.name = "Baz.Foo"
-    assert module._is_wanted_logger(record, wantted_names)
+        self.record.name = "Qax"
+        assert not module._is_wanted_logger(self.record, wantted_names)
 
-    record.name = "Foo"
-    assert not module._is_wanted_logger(record, wantted_names)
+    @unittest.mock.patch("livy.cli.logging._console_formatter")
+    def test_register_highlight_logger(self, fmt):
+        # fail
+        module.register_highlight_logger("foo")
 
-    record.name = "Foo.BarBaz"
-    assert not module._is_wanted_logger(record, wantted_names)
-
-    record.name = "Qax"
-    assert not module._is_wanted_logger(record, wantted_names)
-
-
-def test__get_console_formatter_fallback(stream: typing.TextIO):
-    stream.isatty.return_value = False
-    assert isinstance(module._get_console_formatter(stream), logging.Formatter)
-
-
-@unittest.mock.patch("livy.cli.logging._console_formatter")
-def test_register_highlight_logger(fmt):
-    # fail
-    module.register_highlight_logger("foo")
-
-    # success
-    fmt.highlight_loggers = set()
-    module.register_highlight_logger("foo")
+        # success
+        fmt.highlight_loggers = set()
+        module.register_highlight_logger("foo")
 
 
-def test__LogFilter(record: logging.LogRecord):
-    filter_ = module._LogFilter()
-    filter_.unwanted_loggers.add("Foo")
+class TestFilter(unittest.TestCase):
+    def setUp(self) -> None:
+        self.record = logging.makeLogRecord(
+            {
+                "name": "Test.Bar",
+                "levelno": logging.INFO,
+                "levelname": "INFO",
+                "msg": "Test log message",
+                "created": 1631440284,
+            }
+        )
 
-    record.name = "Test"
-    assert filter_.filter(record)
+        self.filter = module._LogFilter()
+        self.filter.unwanted_loggers.add("Foo.Bar")
 
-    record.name = "Foo.Bar"
-    assert not filter_.filter(record)
+    def test_pass(self):
+        self.record.name = "Test"
+        assert self.filter.filter(self.record)
+
+        self.record.name = "Foo.Baz"
+        assert self.filter.filter(self.record)
+
+        self.record.name = "Foo.BarBaz"
+        assert self.filter.filter(self.record)
+
+    def test_fail(self):
+        self.record.name = "Foo.Bar"
+        assert not self.filter.filter(self.record)
+
+        self.record.name = "Foo.Bar.Baz"
+        assert not self.filter.filter(self.record)
